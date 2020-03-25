76437ed053e46024618eb5dcc811b94f94fe5858
Ans Naveed <49841180+ansn285@users.noreply.github.com>
2020-03-25 15:57:47 +0500

    Add files via upload

---
+++ SchnapMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchnapMove : StateMachineBehaviour
+{
+    private Transform house;
+
+    private float speed;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        house = GameObject.Find("SchnapHouse").GetComponent<Transform>();
+
+        speed = 3;
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, new Vector2(house.position.x, animator.transform.position.y), speed * Time.deltaTime);
+
+        if (animator.transform.position.x == house.position.x)
+        {
+            animator.GetComponent<SpriteRenderer>().enabled = false;
+            GameObject.Find("Schnap").SetActive(false);
+
+            GameObject.Find("GlobalObject").GetComponent<GlobalControl>().schnapInteracted = true;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
