dbb4d6bb5cdb0a0d0b2aa1f492c0cce4909682b7
Ans Naveed <49841180+ansn285@users.noreply.github.com>
2020-03-25 16:05:16 +0500

    Add files via upload

---
+++ Assets/Scripts/Camera/SceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public string sceneName;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Player") { UnitySceneManager.LoadScene(sceneName); }
+
+    }
+
+
+}
