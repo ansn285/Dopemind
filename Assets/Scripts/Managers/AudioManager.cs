a9d5cfa2031cf1346f5bb489b3ff15887e855ec6
Ans Naveed <49841180+ansn285@users.noreply.github.com>
2020-03-25 16:08:52 +0500

    Add files via upload

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using sceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public AudioClip suspenseMusic;
+
+    public AudioClip sereneMusic;
+
+    public AudioClip genericMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (GameObject.Find("Dopermine") && GameObject.Find("Dopermine").GetComponent<FinalState>().isEnemy)
+        {
+            audioSource.Stop();
+            Destroy(gameObject);
+        }
+        
+    }
+
+    public void ChangeAudioClip(AudioClip audioClip)
+    {
+        audioSource.clip = audioClip;
+
+        audioSource.Play();
+    }
+
+}
---
+++ Assets/Scripts/Managers/MenuController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MenuController : MonoBehaviour
+{
+    public Canvas settings;
+    public Canvas keyBindings;
+
+    public AudioMixer audioMixer;
+
+    public Slider volumeSlider;
+
+    private void Awake()
+    {
+        volumeSlider.value = GlobalControl.Instance.volume;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && settings.isActiveAndEnabled && !keyBindings.isActiveAndEnabled)
+        {
+
+            GameObject.Find("Settings").SetActive(false);
+
+        }
+
+        if (Input.GetKey(KeyCode.Escape) && keyBindings.isActiveAndEnabled)
+        {
+
+            GameObject.Find("KeyBindings").SetActive(false);
+
+        }
+
+        
+
+
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+
+        GlobalControl.Instance.volume = volume;
+    }
+
+}
