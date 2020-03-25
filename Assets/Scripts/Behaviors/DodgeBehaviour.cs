99c3536497ad90eb98622161bcc011fc299f7fd2
Ans Naveed <49841180+ansn285@users.noreply.github.com>
2020-03-25 16:04:06 +0500

    Add files via upload

---
+++ Assets/Scripts/Behaviors/DodgeBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DodgeBehaviour : StateMachineBehaviour
+{
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (!animator.GetComponent<CapsuleCollider2D>().enabled && !animator.GetComponent<PlayerMovement>().isFacingLeft)
+        {
+            animator.GetComponent<PlayerMovement>().enabled = false;
+
+            animator.transform.position = new Vector2(animator.transform.position.x + 3 * Time.deltaTime, animator.transform.position.y);
+        }
+
+        else if (!animator.GetComponent<CapsuleCollider2D>().enabled && animator.GetComponent<PlayerMovement>().isFacingLeft)
+        {
+            animator.GetComponent<PlayerMovement>().enabled = false;
+
+            animator.transform.position = new Vector2(animator.transform.position.x - 3 * Time.deltaTime, animator.transform.position.y);
+        }
+
+        else
+        {
+            animator.GetComponent<PlayerMovement>().enabled = true;
+        }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/Scripts/Behaviors/SpearMove.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpearMove : StateMachineBehaviour
+{
+    private Transform player;
+    private Transform npc;
+
+    public float speed;
+
+    [System.NonSerialized] public bool inverse = false;
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.Find("Genrad").transform;
+
+
+        npc = GameObject.Find("Dopermine").transform;
+
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (inverse)
+        {
+            animator.GetComponent<SpriteRenderer>().flipX = false;
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, npc.position, speed * Time.deltaTime);
+        }
+
+        else
+        {
+            animator.GetComponent<SpriteRenderer>().flipX = true;
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, player.position, speed * Time.deltaTime);
+        }
+
+        if (animator.transform.position.x >= npc.position.x)
+        {
+            animator.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+
+            animator.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+
+            inverse = false;
+
+            animator.GetComponent<SpriteRenderer>().flipX = true;
+
+            GameObject.Find("/Dopermine/DoperHitBox").GetComponent<Damage>().TakeDamage(250);
+
+            animator.transform.position = new Vector2(-9.029383f, -2.379735f);
+
+            animator.SetBool("isFollowing", false);
+        }
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
