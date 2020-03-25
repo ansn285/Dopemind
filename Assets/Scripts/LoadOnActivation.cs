4343f42546ac289653fe9effe06936f6cd85ded1
Ans Naveed <49841180+ansn285@users.noreply.github.com>
2020-03-25 16:07:40 +0500

    Add files via upload

---
+++ Assets/Scripts/LoadOnActivation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class LoadOnActivation : MonoBehaviour
+{
+
+    private void OnEnable()
+    {
+        GameObject.Find("GameController").GetComponent<SceneController>().LoadScene("Menu");
+
+    }
+
+}
