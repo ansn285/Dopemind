346c839f9478a5e437d2a10c2585c5bb2ad0d43f
Ans Naveed <49841180+ansn285@users.noreply.github.com>
2020-03-25 15:56:27 +0500

    Add files via upload

---
+++ World.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : SceneController
+{
+    public Transform player;
+
+    public override void Start()
+    {
+        base.Start();
+
+
+
+        if (prevScene == "Scene2" && currentScene != "SchnapHouse" && currentScene != "End")
+        {
+            player.position = new Vector2(0.23f, -2.77f);
+            Camera.main.transform.position = new Vector3(0.23f, -1.67f, -7.4f);
+        }
+        
+        if (prevScene == "SchnapHouse")
+        {
+            player.position = new Vector2(1.84f, -2.77f);
+
+            Camera.main.transform.position = new Vector3(1.84f, -1.67f, -7.4f);
+        }
+
+        if (prevScene == "End" && currentScene == "Scene2")
+        {
+            player.position = new Vector2(7.5f, -2.77f);
+
+            Camera.main.transform.position = new Vector3(7.5f, -1.67f, -7.4f);
+        }
+
+        /*if (prevScene == "End" && currentScene == "Scene1")
+        {
+            player.position = new Vector2(-22.55f, -2.77f);
+
+            Camera.main.transform.position = new Vector3(-22.55f, -1.67f, -7.4f);
+
+        }*/
+    }
+
+
+
+}
